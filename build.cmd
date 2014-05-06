79f4771c2a9e0725b40770cff7442345e6746998
Michael Fink <michael.fink@asamnet.de>
2014-05-06 20:31:37 +0200

    updated build.cmd script

--- build.cmd
+++ build.cmd
@@ -4,33 +4,19 @@ REM
 
 REM -{ config here }-----------------------------
 
-set MSVC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 9.0
-set PLATFORM_SDK_PATH=C:\Program Files\Microsoft SDKs\Windows\v6.0a
-set BOOST_PATH=D:\devel\packages\boost_1_47_0
-set WIX_PATH=D:\devel\tools\wix-v3\bin
-set DOXYGEN_PATH=D:\devel\tools\doxygen\bin
+set MSVC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0
 
 REM -{ config end }------------------------------
 
-REM set up msvc8 env
-call "%MSVC_PATH%\VC\vcvarsall.bat" x86
-
-REM add ATL path
-set INCLUDE=%INCLUDE%;%PLATFORM_SDK_PATH%\Include;%PLATFORM_SDK_PATH%\Include\atl
-set LIB=%LIB%;%PLATFORM_SDK_PATH%\Lib
-
-set INCLUDE=%INCLUDE%;%BOOST_PATH%
+REM set up msvc12 env
+call "%MSVC_PATH%\Common7\Tools\VsDevCmd.bat"
 
 REM build solution
-vcbuild /logfile:build-log.txt /M4 /nohtmllog /rebuild /time /useenv winlame.sln "Release|Win32"
+msbuild /m:4 winlame.sln /property:Configuration=Release /target:Rebuild
 
 REM finished
 echo Finished!
 
 set MSVC_PATH=
-set PLATFORM_SDK_PATH=
-set BOOST_PATH=
-set WIX_PATH=
-set DOXYGEN_PATH=
 
 pause
