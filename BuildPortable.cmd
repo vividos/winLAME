7c6b13235899e5e6e10190e5ae0d0b277f49b420
Michael Fink <michael.fink@asamnet.de>
2016-10-27 21:35:47 +0200

    added more files for winLAME Portable

--- BuildPortable.cmd
+++ BuildPortable.cmd
@@ -23,13 +23,20 @@ copy %ROOT%\source\portable\appinfo.ini winLAMEPortable\App\AppInfo\
 copy %ROOT%\source\winlame\res\winlame.ico winLAMEPortable\App\AppInfo\appicon.ico
 copy %ROOT%\source\portable\appicon*.png winLAMEPortable\App\AppInfo\
 
+mkdir winLAMEPortable\App\AppInfo\Launcher
+copy %ROOT%\source\portable\Splash.jpg winLAMEPortable\App\AppInfo\Launcher\
+copy %ROOT%\source\portable\winLAMEPortable.ini  winLAMEPortable\App\AppInfo\Launcher\
+
 mkdir winLAMEPortable\Data
 copy %ROOT%\source\presets.xml  winLAMEPortable\Data\
 
 mkdir winLAMEPortable\Other
 
 mkdir winLAMEPortable\Other\Source
-copy %ROOT%\docs\Copying.GPL winLAMEPortable\Other\Source\LauncherLicense.txt
+copy %ROOT%\source\portable\AppNamePortable.ini winLAMEPortable\Other\Source\
+copy %ROOT%\source\portable\LauncherLicense.txt winLAMEPortable\Other\Source\
+copy %ROOT%\source\portable\other_source_readme.txt winLAMEPortable\Other\Source\Readme.txt
 REM EULA.txt
+REM copy %ROOT%\docs\Copying.GPL 
 
 cd ..\..
