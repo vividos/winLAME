09eda7e7d0993bc0ed3f4830ea6b8696c7851f48
Michael Fink <michael.fink@asamnet.de>
2016-11-04 16:25:17 +0100

    updated build script to Visual Studio 2015

--- build.cmd
+++ build.cmd
@@ -1,18 +1,21 @@
 @echo off
-REM Creates a winLAME build, HTML help and installer
+REM Creates a winLAME build and installer
 REM
 
 REM -{ config here }-----------------------------
 
-set MSVC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0
+set MSVC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0
 
 REM -{ config end }------------------------------
 
-REM set up msvc12 env
+REM set up msvc14 environment
 call "%MSVC_PATH%\Common7\Tools\VsDevCmd.bat"
 
 REM build solution
 msbuild /m:4 winlame.sln /property:Configuration=Release /target:Rebuild
+
+REM build winLAME Portable
+call BuildPortable.cmd
 
 REM finished
 echo Finished!
