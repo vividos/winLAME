c7e43cc5b5f79de9e501f2c5f62e01c626ded568
Michael Fink <michael.fink@asamnet.de>
2016-11-07 23:02:01 +0100

    moved building docs to cmd file

---
+++ docs/BuildDocs.cmd
@@ -0,0 +1,10 @@
+@echo off
+REM
+REM winLAME - a frontend for the LAME encoding engine
+REM Copyright (c) 2000-2016 Michael Fink
+REM
+REM Runs doxygen to document sourcecode
+REM
+
+doxygen doxygen.cfg
+..\buildtools\hhc docs\html\index.hhp
