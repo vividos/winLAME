57238ef9633a743cb79abacbb1c6c28f07563bf6
Michael Fink <michael.fink@asamnet.de>
2016-11-08 07:24:52 +0100

    simplified portable generation by storing the files in the proper file structure and just using xcopy

--- BuildPortable.cmd
+++ BuildPortable.cmd
@@ -1,3 +1,7 @@
+@echo off
+REM
+REM winLAME - a frontend for the LAME encoding engine
+REM Copyright (c) 2000-2016 Michael Fink
 REM
 REM BuildPortable.cmd - Builds winLAME Portable
 REM
@@ -9,34 +13,16 @@ cd bin\Release
 rmdir /S /Q winLAMEPortable 2> nul
 
 mkdir winLAMEPortable
-REM copy AppNamePortable.exe
-REM copy help.html
 
-mkdir winLAMEPortable\App
+xcopy /S %ROOT%\source\portable\*.* winLAMEPortable\
 
 mkdir winLAMEPortable\App\winLAME
 copy %ROOT%\bin\Release\*.dll winLAMEPortable\App\winLAME\
 copy %ROOT%\bin\Release\*.exe winLAMEPortable\App\winLAME\
 
-mkdir winLAMEPortable\App\AppInfo
-copy %ROOT%\source\portable\appinfo.ini winLAMEPortable\App\AppInfo\
-copy %ROOT%\source\winlame\res\winlame.ico winLAMEPortable\App\AppInfo\appicon.ico
-copy %ROOT%\source\portable\appicon*.png winLAMEPortable\App\AppInfo\
-
-mkdir winLAMEPortable\App\AppInfo\Launcher
-copy %ROOT%\source\portable\Splash.jpg winLAMEPortable\App\AppInfo\Launcher\
-copy %ROOT%\source\portable\winLAMEPortable.ini  winLAMEPortable\App\AppInfo\Launcher\
-
 mkdir winLAMEPortable\Data
 copy %ROOT%\source\presets.xml  winLAMEPortable\Data\
 
-mkdir winLAMEPortable\Other
-
-mkdir winLAMEPortable\Other\Source
-copy %ROOT%\source\portable\AppNamePortable.ini winLAMEPortable\Other\Source\
-copy %ROOT%\source\portable\LauncherLicense.txt winLAMEPortable\Other\Source\
-copy %ROOT%\source\portable\other_source_readme.txt winLAMEPortable\Other\Source\Readme.txt
-REM EULA.txt
-REM copy %ROOT%\docs\Copying.GPL 
-
 cd ..\..
+
+set ROOT=
