995f2e0b66716d0b4cfdd737c1aa49e5f43da53c
Michael Fink <michael.fink@asamnet.de>
2016-11-08 17:59:27 +0100

    cleaned up BuildZip.cmd

--- BuildZip.cmd
+++ BuildZip.cmd
@@ -17,16 +17,14 @@ mkdir zip
 
 copy %ROOT%\bin\Release\*.dll zip\
 copy %ROOT%\bin\Release\*.exe zip\
-copy %ROOT%\source\presets.xml  zip\
+copy %ROOT%\source\presets.xml zip\
 copy "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\redist\1033\vcredist_x86.exe" zip\vcredist_x86_vc14.exe
 
 set ZIP="%ProgramFiles%\7-Zip\7z.exe"
 
-REM Options:
-REM a: add to archive
-REM 
 cd zip
 %ZIP% a ..\..\winLAME-zip.zip *.*
 cd ..\..\..
 
+set ZIP=
 set ROOT=
