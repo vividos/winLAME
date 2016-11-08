2f0365f5a80c89d8cec2580912f9564262179909
Michael Fink <michael.fink@asamnet.de>
2016-11-08 19:13:52 +0100

    added class AudioFileInfoManager

---
+++ source/winlame/AudioFileInfoManager.cpp
@@ -0,0 +1,102 @@
+//
+// winLAME - a frontend for the LAME encoding engine
+// Copyright (c) 2000-2016 Michael Fink
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+/// \file AudioFileInfoManager.cpp
+/// \brief Manager to fetch audio file infos
+//
+#include "stdafx.h"
+#include "AudioFileInfoManager.hpp"
+#include "ModuleManager.h"
+
+AudioFileInfoManager::AudioFileInfoManager()
+   :m_ioService(1), // one thread max.
+   m_upDefaultWork(new boost::asio::io_service::work(m_ioService)),
+   m_stopping(false)
+{
+}
+
+AudioFileInfoManager::~AudioFileInfoManager()
+{
+   Stop();
+   if (m_upThread != nullptr)
+      m_upThread->join();
+}
+
+bool AudioFileInfoManager::GetAudioFileInfo(LPCTSTR filename,
+   int& lengthInSeconds, int& bitrateInKbps, int& sampleFrequencyInHz, CString& errorMessage)
+{
+   ModuleManager& moduleManager = IoCContainer::Current().Resolve<ModuleManager>();
+
+   return moduleManager.getAudioFileInfo(filename, lengthInSeconds, bitrateInKbps, sampleFrequencyInHz, errorMessage);
+}
+
+void AudioFileInfoManager::AsyncGetAudioFileInfo(LPCTSTR filename, AudioFileInfoManager::T_fnCallback fnCallback)
+{
+   ATLASSERT(fnCallback != nullptr);
+
+   if (m_stopping)
+      return;
+
+   if (m_upThread == nullptr)
+   {
+      m_upThread.reset(new std::thread(std::bind(&AudioFileInfoManager::RunThread, boost::ref(m_ioService))));
+   }
+
+   m_ioService.post(
+      std::bind(&AudioFileInfoManager::WorkerGetAudioFileInfo, std::cref(m_stopping), CString(filename), fnCallback));
+}
+
+void AudioFileInfoManager::Stop()
+{
+   m_stopping = true;
+   m_upDefaultWork.reset();
+   m_ioService.stop();
+}
+
+void AudioFileInfoManager::RunThread(boost::asio::io_service& ioService)
+{
+   try
+   {
+      ioService.run();
+   }
+   catch (boost::system::system_error& error)
+   {
+      error;
+      ATLTRACE(_T("system_error: %hs\n"), error.what());
+      ATLASSERT(false);
+   }
+}
+
+void AudioFileInfoManager::WorkerGetAudioFileInfo(const std::atomic<bool>& stopping,
+   const CString& filename, AudioFileInfoManager::T_fnCallback fnCallback)
+{
+   if (stopping)
+      return;
+
+   int lengthInSeconds = 0;
+   int bitrateInKbps = 0;
+   int sampleFrequencyInHz = 0;
+   CString errorMessage;
+
+   bool ret = GetAudioFileInfo(filename, lengthInSeconds, bitrateInKbps, sampleFrequencyInHz, errorMessage);
+
+   if (stopping)
+      return;
+
+   fnCallback(!ret, errorMessage, lengthInSeconds, bitrateInKbps, sampleFrequencyInHz);
+}
---
+++ source/winlame/AudioFileInfoManager.hpp
@@ -0,0 +1,70 @@
+//
+// winLAME - a frontend for the LAME encoding engine
+// Copyright (c) 2000-2016 Michael Fink
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+/// \file AudioFileInfoManager.hpp
+/// \brief Manager to fetch audio file infos
+//
+#pragma once
+
+// includes
+#include <thread>
+#include <atomic>
+
+/// Manager to fetch audio file infos asynchronously
+class AudioFileInfoManager
+{
+public:
+   /// callback function type
+   typedef std::function<void(bool error, const CString& errorMessage, int lengthInSeconds, int bitrateInKbps, int sampleFrequencyInHz)> T_fnCallback;
+
+   /// ctor
+   AudioFileInfoManager();
+
+   /// dtor
+   ~AudioFileInfoManager();
+
+   /// returns infos about audio file; returns false when not supported; synchronous version
+   static bool GetAudioFileInfo(LPCTSTR filename,
+      int& lengthInSeconds, int& bitrateInKbps, int& sampleFrequencyInHz, CString& errorMessage);
+
+   /// retrieves info about audio file; asynchronous vesrion
+   void AsyncGetAudioFileInfo(LPCTSTR filename, T_fnCallback fnCallback);
+
+   /// stops all further processing; no callbacks are called anymore that are not already active
+   void Stop();
+
+private:
+   /// thread function
+   static void RunThread(boost::asio::io_service& ioService);
+
+   /// worker thread function to get audio file infos and to call callback
+   static void WorkerGetAudioFileInfo(const std::atomic<bool>& stopping, const CString& filename, AudioFileInfoManager::T_fnCallback fnCallback);
+
+private:
+   /// worker thread
+   std::unique_ptr<std::thread> m_upThread;
+
+   /// io service
+   boost::asio::io_service m_ioService;
+
+   /// default work for io service
+   std::unique_ptr<boost::asio::io_service::work> m_upDefaultWork;
+
+   /// indicates when manager is currently stopping
+   std::atomic<bool> m_stopping;
+};
