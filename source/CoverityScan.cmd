75247efa92c766ba6b2053637efd72f1a5f2d266
Michael Fink <michael.fink@asamnet.de>
2017-01-11 18:38:57 +0100

    added Coverity scan cmd script

---
+++ source/CoverityScan.cmd
@@ -0,0 +1,18 @@
+@echo off
+REM
+REM winLAME - a frontend for the LAME encoding engine
+REM Copyright (c) 2000-2017 Michael Fink
+REM
+REM runs Coverity analysis build
+REM
+
+rmdir .\cov-int /s /q 2> nul
+del cov-int.zip 2> nul
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
+set PATH=%PATH%;D:\devel\tools\cov-analysis-win64-8.5.0\bin
+
+cov-build --dir cov-int msbuild ..\winlame.sln /property:Configuration=Release /property:Platform=Win32 /target:Rebuild
+"%ProgramFiles%\7-Zip\7z.exe" a cov-int.zip .\cov-int
+
+pause
