0f332cdcc96ea32e61e021ee74e084452ab9fc96
Michael Fink <michael.fink@asamnet.de>
2017-02-27 18:00:24 +0100

    added Appveyor before_build step to get and extract Boost library

---
+++ Boost-Get.ps1
@@ -0,0 +1,18 @@
+#
+# winLAME - a frontend for the LAME encoding engine
+# Copyright (c) 2000-2017 Michael Fink
+#
+# Boost-Get.ps1 - Downloads the Boost version and extracts it, for use in Appveyor winLAME build
+#
+Write-Host "Boost-Get.ps1 - Downloads the Boost version and extracts it"
+Write-Host ""
+
+Add-Type -assembly "System.IO.Compression.Filesystem"
+
+#Write-Host "Downloading Boost.."
+# (New-Object net.webclient).DownloadFile("https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.zip/download?use_mirror=autoselect", "d:\temp\boost_1_63_0.zip")
+
+Write-Host "Extracting archive.."
+[io.compression.zipfile]::ExtractToDirectory("d:\temp\boost_1_63_0.zip", "c:\devel\packages\")
+
+subst z: c:\
