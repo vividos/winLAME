cc23a85f53aca09778b9bc968972e67fc61f40a2
Michael Fink <michael.fink@asamnet.de>
2017-03-24 17:13:11 +0100

    updated SonarQube version to 2.17.1.0 for winLAME 2017 beta 1

--- source/SonarQube.cmd
+++ source/SonarQube.cmd
@@ -27,7 +27,7 @@ rmdir .\bw-output /s /q 2> nul
 
 SonarQube.Scanner.MSBuild.exe begin ^
     /k:"winLAME" ^
-    /v:"1.0" ^
+    /v:"2.17.1.0" ^
     /d:"sonar.cfamily.build-wrapper-output=%CD%\bw-output" ^
     /d:"sonar.host.url=https://sonarqube.com" ^
     /d:"sonar.organization=vividos-github" ^
