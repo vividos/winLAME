160c311de55b062671bbde42e9a51650a1be7971
Michael Fink <michael.fink@asamnet.de>
2018-09-07 23:48:00 +0200

    extended tests for ModuleManager

--- source/winlame/unittest/TestModuleManager.cpp
+++ source/winlame/unittest/TestModuleManager.cpp
@@ -1,6 +1,6 @@
 //
 // winLAME - a frontend for the LAME encoding engine
-// Copyright (c) 2000-2017 Michael Fink
+// Copyright (c) 2000-2018 Michael Fink
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -26,6 +26,10 @@
 #include "ModuleManagerImpl.hpp"
 #include <ulib/IoCContainer.hpp>
 #include "LameNogapInstanceManager.hpp"
+#include <ulib/Path.hpp>
+#include <ulib/unittest/AutoCleanupFolder.hpp>
+#include "resource_unittest.h"
+#include <ulib/win32/ResourceData.hpp>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -87,6 +91,9 @@ namespace unittest
 
             Encoder::InputModule& inputModule = *pInputModule;
 
+            Assert::IsTrue(inputModule.GetLastError().IsEmpty(), _T("last error must be empty after creating new module"));
+            Assert::IsFalse(inputModule.GetModuleName().IsEmpty(), _T("module name must not be empty"));
+
             // getting description must not crash, even when no file was loaded yet
             try
             {
@@ -104,6 +111,92 @@ namespace unittest
                Assert::AreEqual(filter.GetString(), filter2.GetString(), _T("filter strings must match"));
             }
          }
+      }
+
+      /// tests all output modules by accessing and instantiating them
+      TEST_METHOD(TestOutputModules)
+      {
+         Encoder::ModuleManagerImpl moduleManager;
+
+         int countModules = moduleManager.GetOutputModuleCount();
+
+         Assert::IsTrue(countModules > 0, _T("there must be at least one output module"));
+
+         for (int moduleIndex = 0; moduleIndex < countModules; moduleIndex++)
+         {
+            CString name = moduleManager.GetOutputModuleName(moduleIndex);
+            Assert::IsFalse(name.IsEmpty(), _T("output module name name must not be empty"));
+
+            int moduleID = moduleManager.GetOutputModuleID(moduleIndex);
+
+            CString versionText;
+            moduleManager.GetModuleVersionString(versionText, moduleID, 0);
+            Assert::IsFalse(versionText.IsEmpty(), _T("version text must not be empty"));
+
+            Encoder::OutputModule* pOutputModule = moduleManager.GetOutputModule(moduleID);
+
+            Assert::IsNotNull(pOutputModule, _T("output module ptr must not be null"));
+
+            Encoder::OutputModule& outputModule = *pOutputModule;
+
+            Assert::IsTrue(outputModule.GetLastError().IsEmpty(), _T("last error must be empty after creating new module"));
+            Assert::IsFalse(outputModule.GetModuleName().IsEmpty(), _T("module name must not be empty"));
+
+            // getting description must not crash, even when no file was loaded yet
+            try
+            {
+               CString description = outputModule.GetDescription();
+            }
+            catch (...)
+            {
+               Assert::Fail(_T("getting description must not throw an exception"));
+            }
+
+            CString extension = outputModule.GetOutputExtension();
+            Assert::IsFalse(extension.IsEmpty(), _T("output extension string must not be empty"));
+         }
+      }
+
+      /// Tests getting combined filter string
+      TEST_METHOD(TestGetFilterString)
+      {
+         // run
+         Encoder::ModuleManagerImpl moduleManager;
+
+         CString filterstring;
+         moduleManager.GetFilterString(filterstring);
+
+         // check
+         Assert::IsFalse(filterstring.IsEmpty(), _T("filter string must not be empty"));
+      }
+
+      /// Tests getting audio file infos
+      TEST_METHOD(GetAudioFileInfo)
+      {
+         // set up
+         HINSTANCE hInstance = g_hDllInstance;
+         Win32::ResourceData data(MAKEINTRESOURCE(IDR_SAMPLE_MP3), _T("\"RT_RCDATA\""), hInstance);
+
+         UnitTest::AutoCleanupFolder folder;
+
+         CString filename = Path::Combine(folder.FolderName(), _T("sample.mp3")).ToString();
+         data.AsFile(filename);
+
+         // run
+         Encoder::ModuleManagerImpl moduleManager;
+
+         int lengthInSeconds = 0;
+         int bitrateInBps = 0;
+         int samplerateInHz = 0;
+         CString errorMessage;
+
+         moduleManager.GetAudioFileInfo(filename, lengthInSeconds, bitrateInBps, samplerateInHz, errorMessage);
+
+         // check
+         Assert::AreEqual(10, lengthInSeconds, _T("length must be 10 seconds"));
+         Assert::AreEqual(128000, bitrateInBps, _T("bitrate must be 128 kbps"));
+         Assert::AreEqual(44100, samplerateInHz, _T("sample rate must be 44100 Hz"));
+         Assert::IsTrue(errorMessage.IsEmpty(), _T("error message must be empty"));
       }
    };
 
