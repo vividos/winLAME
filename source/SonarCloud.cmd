a5624cb65ec1dfbea18f69f6ae2e03b6b4bfc980
Michael Fink <michael.fink@asamnet.de>
2018-09-08 16:15:12 +0200

    updated version numbers for next beta version

--- source/SonarCloud.cmd
+++ source/SonarCloud.cmd
@@ -1,7 +1,7 @@
 @echo off
 REM
 REM winLAME - a frontend for the LAME encoding engine
-REM Copyright (c) 2000-2017 Michael Fink
+REM Copyright (c) 2000-2018 Michael Fink
 REM
 REM Runs SonarCloud analysis build
 REM
@@ -35,7 +35,7 @@ msbuild winlame.sln /m /property:Configuration=Release,Platform=Win32 /target:Cl
 
 SonarQube.Scanner.MSBuild.exe begin ^
     /k:"winLAME" ^
-    /v:"2.17.4.0" ^
+    /v:"2.18.0.0" ^
     /d:"sonar.cfamily.build-wrapper-output=%CD%\bw-output" ^
     /d:"sonar.host.url=https://sonarcloud.io" ^
     /d:"sonar.organization=vividos-github" ^
