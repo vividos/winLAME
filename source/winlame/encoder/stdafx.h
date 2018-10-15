55b293c71da8262fa9db3ad4caceefe68fde9778
Michael Fink <michael.fink@asamnet.de>
2018-10-15 20:29:45 +0200

    removed WTL NuGet package and includes from encoder project that doesn't use WTL

--- source/winlame/encoder/stdafx.h
+++ source/winlame/encoder/stdafx.h
@@ -1,6 +1,6 @@
 //
 // winLAME - a frontend for the LAME encoding engine
-// Copyright (c) 2000-2017 Michael Fink
+// Copyright (c) 2000-2018 Michael Fink
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -26,7 +26,6 @@
 
 #include <ulib/config/Win32.hpp>
 #include <ulib/config/Atl.hpp>
-#include <ulib/config/Wtl.hpp>
 
 #include "../StdCppLib.hpp"
 #include <boost/noncopyable.hpp>
