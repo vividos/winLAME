ee2d6f5b6fe1fb101a7fdda3943ddb462f63c112
Michael Fink <michael.fink@asamnet.de>
2019-12-20 06:56:07 +0100

    updated version numbers for winLAME 2019 release 1

--- source/SonarCloud.cmd
+++ source/SonarCloud.cmd
@@ -37,7 +37,7 @@ msbuild winlame.sln /m /property:Configuration=SonarCloud,Platform=Win32 /target
 
 SonarScanner.MSBuild.exe begin ^
     /k:"winLAME" ^
-    /v:"2.19.0.0" ^
+    /v:"2.19.1.0" ^
     /d:"sonar.cfamily.build-wrapper-output=%CD%\bw-output" ^
     /d:"sonar.host.url=https://sonarcloud.io" ^
     /o:"vividos-github" ^
