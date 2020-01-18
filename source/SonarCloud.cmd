4a949d7d4b53e5fd8ea5d7866529584f9fd58253
Michael Fink <michael.fink@asamnet.de>
2020-01-18 18:09:25 +0100

    added running unit tests and collecting coverage for SonarQube

--- source/SonarCloud.cmd
+++ source/SonarCloud.cmd
@@ -27,6 +27,15 @@ PATH=%PATH%;%CD%\build-wrapper-win-x86;%CD%\sonar-scanner-msbuild
 popd
 
 REM
+REM Extract OpenCppCoverage build tools
+REM
+pushd ..\buildtools\OpenCppCoverage
+"%ProgramFiles%\7-Zip\7z.exe" x -y -oOpenCppCoverage OpenCppCoverage-x64-0.9.8.0.zip
+copy SonarQube.dll OpenCppCoverage\plugins\exporter
+PATH=%PATH%;%CD%\OpenCppCoverage
+popd
+
+REM
 REM Build using SonarQube scanner for MSBuild
 REM
 cd ..
@@ -39,15 +48,31 @@ SonarScanner.MSBuild.exe begin ^
     /k:"winLAME" ^
     /v:"2.20.0.0" ^
     /d:"sonar.cfamily.build-wrapper-output=%CD%\bw-output" ^
+    /d:"sonar.coverageReportPaths=%CD%\source\winlame-coverage.xml" ^
     /d:"sonar.host.url=https://sonarcloud.io" ^
     /o:"vividos-github" ^
-    /d:"sonar.login=%SONARLOGIN%"
+    /d:"sonar.login=%SONARLOGIN%" ^
+    /d:sonar.cs.vstest.reportsPaths="%CD%\source\TestResults\*.trx"
 if errorlevel 1 goto end
 
 REM
 REM Rebuild Release|Win32
 REM
-build-wrapper-win-x86-64.exe --out-dir bw-output msbuild winlame.sln /m /property:Configuration=SonarCloud,Platform=Win32 /target:Rebuild
+build-wrapper-win-x86-64.exe --out-dir bw-output msbuild winlame.sln /m /property:Configuration=SonarCloud,Platform=Win32 /target:Restore;Rebuild
+
+REM
+REM Run unit tests
+REM
+pushd source
+OpenCppCoverage.exe ^
+   --continue_after_cpp_exception --cover_children ^
+   --sources winlame\encoder ^
+   --export_type SonarQube:winlame-coverage.xml ^
+   --export_type html:CoverageReport ^
+   --modules unittest.dll ^
+   -- "%VSINSTALL%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" ^
+   "..\bin\Release\unittest.dll" /Platform:x86 /InIsolation /logger:trx
+popd
 
 SonarScanner.MSBuild.exe end /d:"sonar.login=%SONARLOGIN%"
 
