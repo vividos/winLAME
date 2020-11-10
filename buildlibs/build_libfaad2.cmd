2cd33b6f084f442b4fe2d64d58bf04d5c42f4aea
Michael Fink <michael.fink@asamnet.de>
2020-11-10 07:48:17 +0100

    updated libfaad2 to version 2.10.0
    added built files, created with Visual Studio 2019 version 16.7.7

--- buildlibs/build_libfaad2.cmd
+++ buildlibs/build_libfaad2.cmd
@@ -1,19 +1,19 @@
 @echo on
 REM
 REM winLAME - a frontend for the LAME encoding engine
-REM Copyright (c) 2000-2019 Michael Fink
+REM Copyright (c) 2000-2020 Michael Fink
 REM
 REM Downloads libfaad2 from github.com/knik0 and compiles it
 REM
 
 REM set this to the filename of the file to download
-set PREFIX=faad2-2_9_1
+set PREFIX=faad2-2_10_0
 
 REM set this to your Visual Studio installation folder
 set VSINSTALL=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community
 
 REM download package
-set URL=https://github.com/knik0/faad2/archive/2_9_1.zip
+set URL=https://github.com/knik0/faad2/archive/2_10_0.zip
 
 if not exist %PREFIX%.zip powershell -Command "& {Invoke-WebRequest -Uri %URL% -Out %PREFIX%.zip}"
 
@@ -25,11 +25,6 @@ REM set up Visual Studio
 call "%VSINSTALL%\Common7\Tools\VsDevCmd.bat"
 
 pushd %PREFIX%\project\msvc
-
-REM update Toolset to v142, in order to use VS2019
-powershell -Command "& {(Get-Content libfaad2_dll.vcxproj) -replace \"v141\",\"v142\" | out-file libfaad2_dll.vcxproj}"
-REM also update framework
-powershell -Command "& {(Get-Content libfaad2_dll.vcxproj) -replace \"8.1\",\"10.0\" | out-file libfaad2_dll.vcxproj}"
 
 REM compile
 msbuild libfaad2_dll.vcxproj /m /property:Configuration=Release,Platform=Win32
