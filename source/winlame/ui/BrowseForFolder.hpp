71c4cce7628c77e0c85e40c5c27642f8edfeddc3
Michael Fink <michael.fink@asamnet.de>
2022-05-21 21:04:19 +0200

    fixed doxygen comment

--- source/winlame/ui/BrowseForFolder.hpp
+++ source/winlame/ui/BrowseForFolder.hpp
@@ -16,7 +16,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-/// \file CommonStuff.hpp
+/// \file BrowseForFolder.hpp
 /// \brief folder-browse function
 /// \details contains code for the folder-browse function
 //
