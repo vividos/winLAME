e68a9cb45732873a28efd2f93cb7228a87f3ed1a
Michael Fink <michael.fink@asamnet.de>
2023-03-12 14:03:29 +0100

    added copying MACDll.dll from vcpkg_installed folder

--- source/libraries/CopyLibraries.cmd
+++ source/libraries/CopyLibraries.cmd
@@ -13,6 +13,9 @@ if "%1" == "" exit 0
 
 mkdir ..\..\bin 2> nul
 
+if "%1" == "Debug" set VCPKG_BIN_DIR=%CD%\..\..\vcpkg_installed\x86-windows\x86-windows\Debug\bin
+if "%1" == "Release" set VCPKG_BIN_DIR=%CD%\..\..\vcpkg_installed\x86-windows\x86-windows\bin
+
 set TARGET=..\..\bin\%1
 
 mkdir %TARGET% 2> nul
@@ -22,6 +25,7 @@ copy basscd.dll %TARGET%
 copy basswma.dll %TARGET%
 copy libmp3lame.dll %TARGET%
 copy libfaac_dll.dll %TARGET%
+copy %VCPKG_BIN_DIR%\MACDll.dll %TARGET%
 
 mkdir %ProgramData%\winLAME 2> nul
 copy ..\presets.xml %ProgramData%\winLAME\presets.xml
