a454105169f4b7adc5523ac3b0f7d26cc918a50e
Michael Fink <michael.fink@asamnet.de>
2026-07-20 22:24:54 +0200

    chore: updated build script for libfaac to version faac-2.0

--- buildlibs/build_libfaac.cmd
+++ buildlibs/build_libfaac.cmd
@@ -1,43 +1,53 @@
-@echo on
+@echo off
 REM
 REM winLAME - a frontend for the LAME encoding engine
-REM Copyright (c) 2000-2019 Michael Fink
+REM Copyright (c) 2000-2026 Michael Fink
 REM
 REM Downloads libfaac from github.com/knik0 and compiles it
 REM
 
 REM set this to the filename of the file to download
-set PREFIX=faac-1_30
+set PREFIX=faac-2.0
 
 REM set this to your Visual Studio installation folder
-set VSINSTALL=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community
+set VSINSTALL=%ProgramFiles%\Microsoft Visual Studio\18\Community
 
 REM download package
-set URL=https://github.com/knik0/faac/archive/1_30.zip
+set URL=https://github.com/knik0/faac/archive/refs/tags/%PREFIX%.zip
 
-if not exist %PREFIX%.zip powershell -Command "& {Invoke-WebRequest -Uri %URL% -Out %PREFIX%.zip}"
+if not exist %PREFIX%.zip (
+	powershell -Command "& {Invoke-WebRequest -Uri %URL% -Out %PREFIX%.zip}"
+)
 
 REM unzip
-rmdir /s /q %PREFIX%\ 2> nul
+rmdir /s /q faac-%PREFIX%\ 2> nul
 "c:\Program Files\7-Zip\7z.exe" x %PREFIX%.zip
 
 REM set up Visual Studio
 call "%VSINSTALL%\Common7\Tools\VsDevCmd.bat"
 
-pushd %PREFIX%\project\msvc
+REM copy project files
+xcopy /S faac\*.* faac-%PREFIX%\
 
-REM update Toolset to v142, in order to use VS2019
-powershell -Command "& {(Get-Content libfaac_dll.vcxproj) -replace \"v141\",\"v142\" | out-file libfaac_dll.vcxproj}"
-REM also update framework
-powershell -Command "& {(Get-Content libfaac_dll.vcxproj) -replace \"8.1\",\"10.0\" | out-file libfaac_dll.vcxproj}"
+pushd faac-%PREFIX%\
+
+echo #define PACKAGE "%PREFIX%" > include\config.h
+echo #define PACKAGE_VERSION "%PREFIX%" >> include\config.h
+echo #define MAX_CHANNELS 8 >> include\config.h
+echo #define FAAC_SBR_DECIMATION 1 >> include\config.h
+echo #define FAACAPI __declspec(dllexport) >> include\config.h
 
 REM compile
 msbuild libfaac_dll.vcxproj /m /property:Configuration=Release,Platform=Win32
+msbuild libfaac_dll.vcxproj /m /property:Configuration=Release,Platform=x64
 
 popd
 
 REM copy artifacts
-copy "%PREFIX%\project\msvc\bin\Release\libfaac_dll.dll" ..\source\libraries\
-copy "%PREFIX%\project\msvc\bin\Release\libfaac_dll.lib" ..\source\libraries\lib\libfaac_dll.lib
+copy "faac-%PREFIX%\include\faac.h" ..\source\libraries\include\
+copy "faac-%PREFIX%\bin\Win32\Release\libfaac_dll.dll" ..\source\libraries\bin\Win32\
+copy "faac-%PREFIX%\bin\Win32\Release\libfaac_dll.lib" ..\source\libraries\lib\
+copy "faac-%PREFIX%\bin\x64\Release\libfaac_dll.dll" ..\source\libraries\bin\x64\
+copy "faac-%PREFIX%\bin\x64\Release\libfaac_dll.lib" ..\source\libraries\lib\x64\
 
 pause
